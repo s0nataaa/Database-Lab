0c1b36fb9b248d970e03d42e05050171f8d1c0cb
s0nataaa <radityaindrasoerachman@gmail.com>
2026-09-05 00:45:34 +0700

    Insert values to database

--- Week-02/Soal-2/order_db.sql
+++ Week-02/Soal-2/order_db.sql
@@ -6,8 +6,8 @@ use order_db;
 CREATE Table `Order` (
     IDOrder VARCHAR(5) PRIMARY KEY,
     NomorMeja VARCHAR(3) NOT NULL,
-    JamMasuk TIME NOT NULL,
-    JamKeluar TIME NOT NULL
+    JamMasuk VARCHAR(5) NOT NULL,
+    JamKeluar VARCHAR(5) NULL
 );
 
 CREATE Table Menu (
@@ -24,6 +24,30 @@ CREATE Table OrderDetail (
     Foreign Key (IDOrder) REFERENCES `Order`(IDOrder),
     Foreign Key (IDMenu) REFERENCES Menu(IDMenu)
 );
+
+INSERT INTO `Order` (`IDOrder`, `NomorMeja`, `JamMasuk`, `JamKeluar`)
+VALUES
+    ('OD001', '1', '10.00', '11.00'),
+    ('OD002', '2', '11.00', '13.00'),
+    ('OD003', '1', '15.00', '15.30'),
+    ('OD004', '1', '15.00', '15.30');
+
+INSERT INTO Menu (`IDMenu`, `NamaMenu`, `Kategori`, `Harga`)
+VALUES
+    ('MN001', 'Nasi Putih', 'Makanan Pokok', 5000),
+    ('MN002', 'Tahu Goreng', 'Lauk', 4000),
+    ('MN003', 'Air Mineral', 'Minuman', 3000),
+    ('MN004', 'Ayam Goreng', 'Lauk', 15000);
+
+INSERT INTO OrderDetail (`IDOrder`, `IDMenu`)
+VALUES
+    ('OD001', 'MN001'),
+    ('OD001', 'MN002'),
+    ('OD001', 'MN003'),
+    ('OD002', 'MN001'),
+    ('OD002', 'MN004'),
+    ('OD003', 'MN002'),
+    ('OD004', 'MN004');
 
 --Helping Command--
 SHOW TABLES;
